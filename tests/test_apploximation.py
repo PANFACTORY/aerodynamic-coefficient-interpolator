0693467cbeb545e63ca066f0b1554063f60a7cec
PANFACTORY <yutatanabe510@gmail.com>
2023-04-23 11:14:28 +0900

    ♻️ テスト名等の修正

--- tests/test_apploximation.py
+++ tests/test_apploximation.py
@@ -1,19 +1,17 @@
-from pandas import DataFrame
+import math
 
 from src.apploximate_function import AlphaRe15
 from src.xfoil_api import XFoilApi
 
 
-def test_xfoil_api() -> None:
+def test_apploximation() -> None:
     df_foil = XFoilApi.load_foil("./tests/DAE21.dat")
     api = XFoilApi("./xfoil.exe")
-
-    df_analysis = api.analyze(df_foil, 0.0, 10.0, 1.0, 200000.0, 700000.0, 50000.0)
+    df_analysis = api.analyze(df_foil, 0.0, 10.0, 0.5, 200000.0, 700000.0, 50000.0)
     model = AlphaRe15(df_analysis)
 
     re = 530000.0
     df_analysis_check = api.analyze(df_foil, 0.0, 10.0, 1.0, re, re, 1.0)
-    df_base = DataFrame(data=[[a, re] for a in range(0, 11, 1)], columns=["alpha", "Re"])
-
-    print(df_analysis_check)
-    print(model.predict(df_base))
+    df_predict = model.predict(df_analysis_check[["alpha", "Re"]])
+    error = math.sqrt(pow(df_predict["CL"] - df_analysis_check["CL"], 2).sum())
+    assert error < 0.05
